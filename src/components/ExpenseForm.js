76bdcb2fe1efadae529c5bc0772d5c67dd37f96a
[Abid] <[abid.hassan0801@gmail.com]>
2021-06-09 19:13:30 +0600

    first commit

---
+++ src/components/ExpenseForm.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const ExpenseForm = () => {
+    return (
+        <div>
+            Hello from expense form
+        </div>
+    )
+}
+export default ExpenseForm;
---
+++ src/components/ExpenseList.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Abid from "./ExpenseItem";
+
+
+export const ExpenseList = () => {
+    return (
+        <div>
+            Hello from ExpenseList
+            <Abid/>
+        </div>
+        
+    )
+}
+export default ExpenseList;
